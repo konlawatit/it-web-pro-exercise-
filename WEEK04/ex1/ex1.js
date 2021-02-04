0c12c0957222ad40cf1d158e0a5f359cfe31a51d
bb0b <konlawatit@gmail.com>
2021-02-04 20:47:16 +0700

    Week4: Vue.JS

---
+++ WEEK04/ex1/ex1.js
@@ -0,0 +1,11 @@
+
+var app1 = new Vue({
+    el: '#ex1',
+    data: {
+        student_id: 62070007,
+        student_fname: "Konlawat",
+        student_lname: "Hutsaithong",
+        isList: 'circle',
+        test: 'red'
+    }
+})
